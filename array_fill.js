2c7fdb7321d303d76cbf67eaaddebf57d79e7c6c
Stefan Stefanov <stefan@localhost.localdomain>
2017-09-06 15:22:07 +0300

    Adding array_fill.js

---
+++ array_fill.js
@@ -0,0 +1,13 @@
+// Creating empty array
+var arr = [];
+
+for(var i = 0; i < 100; i++){
+  // i - from 0 to 99
+
+  // The element at position i becomes value of... i
+  arr[i] = i;
+}
+
+console.log("The length: ", arr.length);
+console.log("The array: ", arr);
+
